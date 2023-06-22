178d9e89b869af0c81ff464ddee6ce986beb3116
Muhtasim <muhtasimfuad625@gmail.com>
2023-06-22 18:31:43 +0600

    twentysecondjuneeve

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,44 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
 import { User, Bookmark } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { AuthDto } from './dto'
 import * as argon from 'argon2'
 import { stringify } from 'querystring'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 
 @Injectable()
 
-export class AuthService{
-    constructor(private prisma: PrismaService) {}
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
 
     async signup(dto: AuthDto) {
         const hash = await argon.hash(dto.password)
 
-        const user = await this.prisma.user.create({
-            data: {
-                name: dto.name,
-                guardName: dto.guardName,
-                deptName: dto.deptName,
-                role: 0,
-                email: dto.email,
-                password: hash
-            },
-            select: {
-                id: true,
-                name: true,
-                deptName: true,
-                email: true,
-                role: true
-            }
-        });
+        try {
+            const data = await this.prisma.user.findUnique({
+                where: {
+                    email: dto.email,
+                }
+            });
+            if(data) return ({ msg: "email already do exist" })
 
-        return user;
-        // return 'SIGNUPHERE'
+            const user = await this.prisma.user.create({
+                data: {
+                    name: dto.name,
+                    guardName: dto.guardName,
+                    deptName: dto.deptName,
+                    role: 0,
+                    email: dto.email,
+                    password: hash
+                }
+            });
+            delete user.password;
+
+            return user;
+        }catch(error) {
+            return ({ msg: "email exists" })
+
+        }
     }
     signin() {
         return 'SIGNINHERE'
