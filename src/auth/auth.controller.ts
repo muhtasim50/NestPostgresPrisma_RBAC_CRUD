2da9ee5e22cd43271e091616ab1b1cb13e56a0f0
Muhtasim <muhtasimfuad625@gmail.com>
2023-06-22 12:35:06 +0600

    twentysecondjune

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 
 @Controller('auth')
@@ -8,8 +9,12 @@ export class AuthController{
     constructor(private authService: AuthService) {}
 
     @Post('signup')
-    signup() {return this.authService.signup()}
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup()
+    }
 
     @Post('signin')
-    signin() {return this.authService.signin()}
+    signin() {
+        return this.authService.signin()
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { User, Bookmark } from '@prisma/client'
+import { PrismaService } from 'src/prisma/prisma.service'
 
-@Injectable({})
+@Injectable()
 
 export class AuthService{
+    constructor(private prisma: PrismaService) {}
     signup() {return 'SIGNUPHERE'}
     signin() {return 'SIGNINHERE'}
 }
